097b4d568430a3fcc6fcef16d286ff4c787ce3e6
Gittysquig <57458790+Gittysquig@users.noreply.github.com>
2020-05-11 11:37:50 +0100

    Create The De-Coder.py

---
+++ The De-Coder.py
@@ -0,0 +1,20 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = int(input('Input Key:'))
+
+
+
+newmessage = ''
+
+message = input('Please Input Message:')
+
+for character in message:
+  if character in alphabet:
+     position = alphabet.find(character)
+     newposition = (position - key) %26
+     newcharacter = alphabet[newposition]
+     newmessage += newcharacter
+  else:
+    newmessage += character 
+
+print'The Coded Message is:', newmessage
+
